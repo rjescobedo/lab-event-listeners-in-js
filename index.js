6867bce005a1a30057db1d001ae37429f4fcb8f9
Raul Escobedo <rjescobedo@gmail.com>
2025-01-08 07:34:27 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-// Handle Button Clicks
+//Declaring global variables
+const body = document.querySelector('body');
+const keyPressDisplay = document.querySelector('#keyPressDisplay');
+const textInput = document.querySelector('#textInput');
+const textInputDisplay = document.querySelector('#textInputDisplay');
 
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+  body.classList.add('blue');
+  console.log('clicked')
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  body.classList.remove('blue');
+  console.log('dblclicked')
 }
 
 // Capture Keyboard Input
@@ -15,6 +23,8 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  keyPressDisplay.textContent = event.key
+  console.log(event.key)
 }
 
 // Process Text Input
@@ -22,6 +32,8 @@ function displayKeyPress(event) {
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  textInputDisplay.textContent = textInput.value;
+  console.log(textInput.value);
 }
 
 // Attach Event Listeners
